82ca7173a21d519bb5cfa868800d4f47a1bbf711
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-05-27 23:48:30 +0100

    Sorrow...

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -1,51 +1,55 @@
-// import Link from "next/link";
-// import { BannerData } from "./interfaces";
-// import Image from "next/image";
+import Link from "next/link";
+import { BannerData } from "./interfaces";
+import Image from "next/image";
 
-// type FooterBannerProps = {
-//   footerBanner: BannerData;
-// };
+type FooterBannerProps = {
+  footerBanner: BannerData;
+};
 
-// function FooterBanner({
-//   footerBanner: {
-//     discount,
-//     saleTime,
-//     desc,
-//     largeText,
-//     largeText2,
-//     image,
-//     smallText,
-//     product,
-//     buttonText,
-//     midText,
-//   },
-// }: FooterBannerProps) {
-//   return (
-//     <div className="footer-banner-container">
-//       {/* <div className="banner-desc">
-//         <div className="left">
-//           <p>{discount}</p>
-//           <h3>{largeText}</h3>
-//           <h3>{largeText2}</h3>
-//         </div>
-//         <div className="right">
-//           <p>{smallText}</p>
-//           <h3>{midText}</h3>
-//           <h3>{desc}</h3>
-//           <Link href={`/product/${product}`}>
-//             <button type="button">{buttonText}</button>
-//           </Link>
-//         </div>
-//         <Image
-//           src={`/images${image}`}
-//           className="footer-banner-image"
-//           width={200}
-//           height={200}
-//           alt={product}
-//         />
-//       </div> */}
-//     </div>
-//   );
-// }
+function FooterBanner(
+  {
+    /*
+  footerBanner: {
+    discount,
+    saleTime,
+    desc,
+    largeText,
+    largeText2,
+    image,
+    smallText,
+    product,
+    buttonText,
+    midText,
+  },
+}: FooterBannerProps*/
+  }
+) {
+  return (
+    <div className="footer-banner-container">
+      {/* <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText}</h3>
+          <h3>{largeText2}</h3>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <h3>{desc}</h3>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+        <Image
+          src={`/images${image}`}
+          className="footer-banner-image"
+          width={200}
+          height={200}
+          alt={product}
+        />
+      </div> */}
+    </div>
+  );
+}
 
-// export default FooterBanner;
+export default FooterBanner;
